1a99c78745d2bd71499136c91a649df86527e39d
João Vitor <141925590+ichigogold@users.noreply.github.com>
2025-11-25 23:24:56 -0300

    Update script.js
    KKzkzkakkzakksksksksl

--- script.js
+++ script.js
@@ -7,18 +7,13 @@ const gameOverText = document.getElementById("gameover");
 const startImage = document.getElementById("startImage");
 const phone = document.getElementById("phone");
 const voice = document.getElementById("voice");
+const restartBtn = document.getElementById("restartBtn");
 
-let currentRoom = "";    
+let currentRoom = "";
 let mobRoom = "";
 let mobVisible = false;
 let reacting = false;
 let gameOver = false;
-
-const roomBackgrounds = {
-  esquerda: "janela noite.jpeg",
-  meio: "meioescuro.jpeg",
-  direita: "q.jpg"
-};
 
 startBtn.onclick = () => {
   menu.style.display = "none";
@@ -37,6 +32,7 @@ startBtn.onclick = () => {
     }, 1000);
   }, 2000);
 };
+
 function mostrarTelefone() {
   phone.style.display = "block";
   setTimeout(() => phone.style.opacity = 1, 100);
@@ -63,6 +59,13 @@ function mostrarTelefone() {
 function changeRoom(room) {
   if (gameOver) return;
   currentRoom = room;
+
+  const roomBackgrounds = {
+    esquerda: "janela noite.jpeg",
+    meio: "meioescuro.jpeg",
+    direita: "q.jpg"
+  };
+
   game.style.background = `url('${roomBackgrounds[room]}') center/cover no-repeat`;
 }
 
@@ -71,6 +74,7 @@ function spawnMob() {
 
   const rooms = ["esquerda", "meio", "direita"];
   mobRoom = rooms[Math.floor(Math.random() * 3)];
+
   mobVisible = true;
   mob.style.display = "block";
   mob.style.opacity = mobRoom === currentRoom ? 1 : 0;
@@ -111,11 +115,8 @@ function usarFlash() {
 
 function triggerGameOver() {
   gameOver = true;
+
   mob.style.display = "block";
-  mob.style.width = "100%";
-  mob.style.height = "100%";
-  mob.style.left = "0";
-  mob.style.background = "url('inimigo.jpeg') center/contain no-repeat";
   gameOverText.style.display = "flex";
 }
 
@@ -126,6 +127,7 @@ function gameLoop() {
 
 document.addEventListener("keydown", (e) => {
   if (gameOver) return;
+
   const key = e.key.toLowerCase();
 
   switch (key) {
@@ -149,3 +151,17 @@ document.addEventListener("keydown", (e) => {
       break;
   }
 });
+
+restartBtn.onclick = () => {
+  gameOverText.style.display = "none";
+  game.style.display = "none";
+  menu.style.display = "flex";
+
+  mob.style.display = "none";
+  gameOver = false;
+  currentRoom = "";
+  reacting = false;
+  mobVisible = false;
+
+  game.style.background = "url('basE.jpeg') center/cover no-repeat";
+};
